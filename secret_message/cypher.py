cd93c53e0015da40cc5a8f9d60fd7ab6fe07b694
henry <hnerybtaylor@icloud.com>
2019-03-14 15:53:08 -0700

    secret agent chat

---
+++ secret_message/cypher.py
@@ -0,0 +1,6 @@
+alphabet= 'abcdefghijklmnopqrstuvwxyz'
+print(alphabet[0:3])
+key = 12
+character = input('Please enter a character. now!!! ')
+position= alphabet.find(character)
+newPosition = position + key
