de43cc68f7320af0e4f031b3510234f9675428ce
henry <henrybtaylor@icloud.com>
2019-03-21 15:27:43 -0700

    moved

--- secret_message/cypher.py
+++ secret_message/cypher.py
@@ -1,18 +1,29 @@
 alphabet= 'abcdefghijklmnopqrstuvwxyz'
+
+
+def encrypt(message,key):
+    newMessage=""
+    for character in message:
+        if character in alphabet:
+
+            position= alphabet.find(character)
+            newPosition = (position + key) %26
+            newCharacter= alphabet[newPosition]
+            #print(character, "is now", newCharacter)
+            newMessage += newCharacter
+        else:
+            newMessage += character
+    return newMessage
+
+
 key = int(input('enter number to be key '))
-newMessage=""
 
 message = input(' please enter message!!!!!!!!!!!! ')
 
-for character in message:
-    if character in alphabet:
 
-        
-        position= alphabet.find(character)
-        newPosition = (position + key) %26
-        newCharacter= alphabet[newPosition]
-        #print(character, "is now", newCharacter)
-        newMessage += newCharacter
-    else:
-        newMessage += character
-print(newMessage)
+encrypted = encrypt(message,key)
+
+print(encrypted)
+
+decrypted = encrypt(encrypted,key * -1)
+print(decrypted)
