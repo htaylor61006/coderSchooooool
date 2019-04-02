5a51d593a00491bb28044a4a0a92a7f4a500b15e
henry <henrybtaylor@icloud.com>
2019-04-02 15:55:41 -0700

    started menu

--- oneTimePad/gen_sheets.py
+++ oneTimePad/gen_sheets.py
@@ -43,5 +43,55 @@ def encrypt(plaintext,sheet):
 
     return cipher_text
 
+def decrypt(cipherText,sheet):
+    plain_text=''
+    for position,character in enumerate(cipherText):
+        if character not in ALPHABET:
+            plain_text+= character
+        else:
+            decrypted= (ALPHABET.index(character) - int(sheet[position])) % 26
+            plain_text+= ALPHABET[decrypted]
+
+    return plain_text 
+            
+
+def menu():
+    choices=['1','2','3','4']
+    choice=(0)
+    while True:
+        while choice not in choices:
+            print('What would you like to do?')
+            print('1. Generate one-time pads')
+            print('2. Encrypt a message')
+            print('3. Decrypt a message')
+            print('4. Quit the program')
+            choice = input('Please type 1,2,3 or 4 and press Enter ')
+
+
+    #need to compleate function
+
+
+            
+generate_otp(5,100)
+sheet=load_sheet('otp3.txt')
+msg=get_plain_text()
+cypher_text=encrypt(msg,sheet)
+print(cypher_text)
+#plain_text= decrypt(cypher_text,sheet)
+sheet_guess = input("which otp to use?: ")
+current_sheet = 'otp' + sheet_guess + '.txt'
+sheet = load_sheet(current_sheet)
+plain_text = decrypt(cypher_text,sheet)
+print(plain_text)
+
+
+
+
+
+
+
+
+
+
 
 
